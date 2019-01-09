736590c22b0d5431b6ff54b9c369a190a7049ebf
IlyaOsotov <soten58@yandex.ru>
2019-01-10 00:49:50 +0300

    optimized number of threads

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ server '159.69.216.57', port: 22, roles: [:web, :app, :db], primary: true
 set :repo_url,        'git@github.com:IlyaOsotov/vue-rails-todo.git'
 set :application,     'vue-rails-todo'
 set :user,            'deploy'
-set :puma_threads,    [16, 16]
+set :puma_threads,    [4, 12]
 set :puma_workers,    1
 
 # Don't change these unless you know what you're doing
