94abdaec06293f815c22195efe1b4bc73e657200
IlyaOsotov <soten58@yandex.ru>
2019-01-10 00:29:55 +0300

    try to increase threads

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ server '159.69.216.57', port: 22, roles: [:web, :app, :db], primary: true
 set :repo_url,        'git@github.com:IlyaOsotov/vue-rails-todo.git'
 set :application,     'vue-rails-todo'
 set :user,            'deploy'
-set :puma_threads,    [4, 16]
+set :puma_threads,    [12, 12]
 set :puma_workers,    4
 
 # Don't change these unless you know what you're doing
